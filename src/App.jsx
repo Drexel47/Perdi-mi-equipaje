a6dd99bff97a13345b1c27db6cec1ee401dafdba
Ignacio Pizarro <30320437+Drexel47@users.noreply.github.com>
2025-11-03 19:27:41 -0300

    Actualización css del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.scss';
+import './App.css';
 
 
 
@@ -13,20 +13,23 @@ function App() {
       <div className="main">
         <header className="header">
           <nav className="navegacion-principal">
-                
-            <div className="nav-opc">
+            <div className="nombre-pagina">
+              <h2>Perdí mi equipaje</h2>
+            </div>
+            <div className="navegacion__principal">
               <a href="">Ingresa</a>
               <a href="">Registrate</a>
-            
+
             </div>
+            
+            
+            
           </nav>
         </header>
         <main className="contenido-principal">
           <div className="contenedor">
             <section className="banner">
-              <div className="banner-texto">
-                <h2>Perdí mi equipaje</h2>
-              </div>
+  
               <div className="banner-desc">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequuntur obcaecati doloribus commodi dolor qui facilis harum autem. Sit amet deleniti quos assumenda velit adipisci impedit. Praesentium dolor saepe delectus?</p>
               </div>
@@ -34,22 +37,25 @@ function App() {
             </section>
             <section className="contacto">
               <h3 className="subtitulo">Tienes Dudas? Contactanos</h3>
-                <form>
-                  <fieldset>
+                <form className='formulario'>
+                  
+                <fieldset>
                     <label >Nombre</label>
-                    <input type="text" placeholder="Nombre y Apellido"></input>
+                    <input type="text"  placeholder="Nombre y Apellido"></input>
                     <label >Correo Electronico</label>
                     <input type="email" placeholder="E-mail"></input>
                     <label >Número de Contacto</label>
                     <input type="tel"></input>
                     <label >Escribe Aquí</label>
                     <input type="text" name="" id=""></input>
+                    <input className="btn-primario btn-consulta" type="button" value="Enviar"></input>   
+                </fieldset>    
 
-                        <input type="button" value="Enviar"></input> 
+                    
 
 
 
-                  </fieldset>
+                 
 
 
                 </form>
